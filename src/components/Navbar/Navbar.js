4b931a9c30c7c2807c2b36ccbf1f9c1c99b947c5
Calvin Kinateder <calvinkinateder@gmail.com>
2022-11-11 09:58:55 -0500

    navbar update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import WbSunnyRoundedIcon from '@material-ui/icons/WbSunnyRounded'
 import MenuIcon from '@material-ui/icons/Menu'
 import CloseIcon from '@material-ui/icons/Close'
 import { ThemeContext } from '../../contexts/theme'
-import { projects, skills, contact } from '../../portfolio'
+import { projects, skills, experience, contact } from '../../portfolio'
 import './Navbar.css'
 
 const Navbar = () => {
@@ -27,6 +27,18 @@ const Navbar = () => {
               className='link link--nav'
             >
               Projects
+            </a>
+          </li>
+        ) : null}
+
+        {experience.length ? (
+          <li className='nav__list-item'>
+            <a
+              href='#experience'
+              onClick={toggleNavList}
+              className='link link--nav'
+            >
+              Experience
             </a>
           </li>
         ) : null}
